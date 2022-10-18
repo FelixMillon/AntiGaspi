38a8bcf79becea2556da1fccbb6b82771d12345e
Kerraz <mohamedreda.kerraz@gmail.com>
2022-10-18 12:28:30 +0200

    Ajout page de base

---
+++ enquete_satisfaction/js/front.js
@@ -0,0 +1,19 @@
+
+
+function changePage(page) {
+    switch(page){
+        case 'home' :
+        window.location = "index.html/home.html";
+        window.location.href = "https://www.google.com";
+        break;
+        case 'page_1' :
+            window.location = "https://www.google.com";
+            window.location.href = "https://www.google.com";
+            break;
+        case 'page_2' :
+        window.location = "https://www.google.com";
+        window.location.href = "https://www.google.com";
+        break;
+    }
+
+}
