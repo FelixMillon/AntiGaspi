a928251ae8bff3a280a3b56bf77e4674de345fb4
Kerraz <mohamedreda.kerraz@gmail.com>
2022-10-21 21:56:31 +0200

    html header footer v1

--- enquete_satisfaction/js/front.js
+++ enquete_satisfaction/js/front.js
@@ -1,19 +1,24 @@
 
+// selecteur de page 
+
 
 function changePage(page) {
     switch(page){
         case 'home' :
         window.location = "index.html/home.html";
-        window.location.href = "https://www.google.com";
+        window.location.href = "../html/home.html";
         break;
         case 'page_1' :
-            window.location = "https://www.google.com";
-            window.location.href = "https://www.google.com";
+            window.location = "index.html/page_1.html";
+            window.location.href = "../html/page_1.html";
             break;
         case 'page_2' :
-        window.location = "https://www.google.com";
-        window.location.href = "https://www.google.com";
+        window.location = "index.html/page_2.html";
+        window.location.href = "../html/page_2.html";
         break;
     }
 
 }
+
+// Resolution
+
