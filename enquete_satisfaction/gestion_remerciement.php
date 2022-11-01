3ad4ab66490f8e5103c5e8440a7902939055d40a
felix <felix.millon@outlook.com>
2022-11-01 02:05:50 +0100

    bdd gestion des avis enquete et trigger numquestion

---
+++ enquete_satisfaction/gestion_remerciement.php
@@ -0,0 +1,6 @@
+<?php
+	$unControleur->setTable("enquete"); //se mettre sur la bonne table
+	$unControleur->setTable("sujet"); //se mettre sur la bonne table
+    require_once("vue/remerciement.php");//appel ta page
+	
+?>
