62c056e84db32d1290614ea26fbda5373344e668
Kerraz <mohamedreda.kerraz@gmail.com>
2023-05-10 14:35:16 +0200

    Correction inscription

--- firedrive/Firecrest Api/inscription.php
+++ firedrive/Firecrest Api/inscription.php
@@ -3,7 +3,7 @@
 	require_once ("controleur/controleur.class.php");
 	require_once ("modele/config_bdd.php");
 
-	if (isset($_REQUEST['email']) && isset($_REQUEST['mdp']) && isset($_REQUEST['nom'])  && isset($_REQUEST['prenom'])  && isset($_REQUEST['tel']) && isset($_REQUEST['numrue']) && isset($_REQUEST['rue']) && isset($_REQUEST['ville'])&& isset($_REQUEST['cp']) )
+	if (isset($_REQUEST['email']) && isset($_REQUEST['mdp']) && isset($_REQUEST['nom'])  && isset($_REQUEST['prenom'])  && isset($_REQUEST['tel']) )
 	{
 
 
@@ -15,10 +15,10 @@
 		"date_inscription"=>null, 
 		"noteconfemp"=>null, 
 		"tel"=>$_REQUEST['tel'], 
-		"numrue"=>$_REQUEST['numrue'], 
-		"rue"=>$_REQUEST['rue'], 
-		"ville"=>$_REQUEST['ville'], 
-		"cp"=>$_REQUEST['cp'],
+		"numrue"=>'numrue', 
+		"rue"=>'rue', 
+		"ville"=>'ville', 
+		"cp"=>'cp',
 		"id_vehicule"=>null,
 		"notepublic"=>5,
 		"valide"=>null);
